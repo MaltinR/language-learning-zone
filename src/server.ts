a73215b97efb3df9bafdebf172e6b2c28ebe1965
Martin <martinlam@wisegrande.com>
2026-02-13 17:07:21 +0800

    Backend now can read frontend files

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import path from "path";
 import apiRouter from "./web/routers/apiRouter";
 
 const app = express();
@@ -6,6 +7,16 @@ const port = 1668;
 
 app.use(express.json());
 app.use("/api", apiRouter());
+
+// Serve static React files
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, "frontend", "dist"))); // or "build"
+
+// Catch-all: send index.html for React routes
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
+
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}.`);
