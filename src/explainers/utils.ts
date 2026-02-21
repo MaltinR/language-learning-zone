4f5c2da8073a515452bc9ce80fd12b66abe41c8d
Martin Lam <martinlam33@gmail.com>
2026-02-21 14:36:53 +0800

    Added log to debug prompt

--- src/explainers/utils.ts
+++ src/explainers/utils.ts
@@ -1,3 +1,5 @@
 export function fromTemplate(promptTemplate: string, textLang: string, explainLang: string): string {
-    return promptTemplate.replaceAll("{textLang}", textLang).replaceAll("{explainLang}", explainLang);
+    const prompt = promptTemplate.replaceAll("{textLang}", textLang).replaceAll("{explainLang}", explainLang);
+    console.log("prompt", prompt);
+    return prompt;
 }
